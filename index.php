49c80c970648803ae7af33fa2a0fa4e921009bd3
Lisa Trevis <l_trevis@hotmail.com>
2017-06-17 13:44:32 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+	
+	<head>
+		
+		<meta charset="utf-8">
+
+		<meta name="description" content="A fine gardening and lawn conversion service focusing on: raised herb and vegetable beds, bee & butterfly habitats, eco-friendly mulch, and drought-tolerant and native plants.">
+
+		<meta name="keywords" content="gardening, landscaping, butterfly, bees, honeybees, vegetables, herbs, mulch, organic, aquaponics">
+
+		<meta name="author" content="Lisa Trevis">
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>There She Grows CA - Home</title>
+
+		<link rel="shortcut icon" href="images/world.png">
+		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<link rel="stylesheet" href="css/main.css">
+	
+	</head>
+	
+	<body>
+
+		<header>
+			
+			<div class="frame">
+				<h1>Specializing in lawn conversion<br>with a focus on:</h1>
+				<ul>
+					<li>raised herb &amp; vegetable beds</li>
+					<li>bee and butterfly habitats</li>
+					<li>eco-friendly mulch</li>
+					<li>drought-tolerant and native plants</li>
+				</ul>
+			</div>
+
+		</header>
+
+		<nav class="container-fluid">
+
+			<div class="row">			
+				
+				<div class="col-md-4">
+					<h1>There She Grows CA</h1>
+				</div>
+
+				<ul class="col-md-8">
+					<li><a href="index.php" target="_blank">HOME</a></li>
+					<li><a href="about.php" target="_blank">ABOUT</a></li>
+					<li><a href="services.php" target="_blank">SERVICES</a></li>
+					<li><a href="faq.php" target="_blank">FAQ</a></li>
+					<li><a href="contact.php" target="_blank">CONTACT</a></li>
+				</ul>
+			</div>
+		</nav>
+
+		<footer id="contact" class="row">
+
+			<div class="contact col-md-4">
+				<h5>Contact</h5>
+				<form>
+					<input type="text" placeholder="Name"><br>
+					<input type="email" placeholder="Email"><br>
+					<textarea placeholder="Message"></textarea><br>
+				</form>	
+				<button>Send</button><br>
+				<p>&copy;2017 ThereSheGrowsCA</p>
+			</div>
+
+			<div class="map col-md-4">
+
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d50469.8854899033!2d-122.31340691813635!3d37.75797225291655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f80d8f2cf1595%3A0x66ff99cf60016f14!2sAlameda%2C+CA!5e0!3m2!1sen!2sus!4v1485395112477" width="300" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+				
+				<p>Alameda, CA 94501<br>424-2GROW-CA<br>(424-247-6922)</p>
+
+			</div>
+
+			<div class="social col-md-4">
+				
+				<ul class="social-icons">
+
+					<a href="https://www.etsy.com/shop/ThereSheGrowsCA" target="_blank">
+						<i class="fa fa-etsy fa-2x" aria-hidden="true"></i>
+					</a>
+
+					<a href="https://twitter.com/thereshegrowsca" target="_blank">
+						<i class="fa fa-twitter fa-2x" aria-hidden="true"></i>
+					</a>
+
+					<a href="https://www.instagram.com/thereshegrowsca" target="_blank">
+						<i class="fa fa-instagram fa-2x" aria-hidden="true"></i>
+					</a>
+
+					<a href="https://facebook.com/thereshegrowsca" target="_blank">
+						<i class="fa fa-facebook fa-2x" aria-hidden="true"></i>
+					</a>
+
+				</ul>
+
+				<p class="nav">NAVIGATION</p>
+
+				<ul class="navbar">
+					<li><a href="index.php" target="_blank">HOME</a></li>
+					<li><a href="about.php" target="_blank">ABOUT</a></li>
+					<li><a href="services.php" target="_blank">SERVICES</a></li>
+					<li><a href="faq.php" target="_blank">FAQ</a></li>
+					<li><a href="contact.php" target="_blank">CONTACT</a></li>
+				</ul>
+
+			</div>
+
+		</footer>
+
+	</body>
+
+</html>
